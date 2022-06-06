2b1c0594ca7d2290b83708dd5dbc5872b62654ee
ignacio.ambia <ignacio@bego.ai>
2022-06-06 11:34:48 -0500

    add smal-resolution & incompatible-browser modals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,20 @@ import { map } from "rxjs/operators";
 import { AlertService } from "./shared/services/alert.service";
 import * as AOS from "aos";
 
+interface IncompatibleBrowserVersion {
+  /**
+   * The name of the navigator that is not compatible
+   */
+  navigator: String;
+  /**
+   * Min browser version accepted
+   */
+  minVersion?: number;
+  /**
+   * Max browser version accepted
+   */
+  maxVersion?: number;
+}
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -17,6 +31,20 @@ export class AppComponent {
 
   alert$: Observable<any> = this.alertService.alert$;
 
+  showSmallResolutionModal: boolean = false;
+  incompatibleBrowsers: IncompatibleBrowserVersion[] = [
+    {
+      navigator: 'msie',
+      minVersion: 11
+    }
+  ];
+  showIncompatibleBrowserModal: boolean = false;
+
+  minWidthResolution = 1024;
+
+
+
+
   constructor(
     private languageService: LanguageService,
     private alertService: AlertService,
@@ -24,6 +52,33 @@ export class AppComponent {
   ) {
     languageService.setInitialLanguage();
     profileInfoService.getProfilePic();
+
+
+    this.showSmallResolutionModal = window.innerWidth < this.minWidthResolution;
+    window.addEventListener('resize', () => {
+      this.showSmallResolutionModal =
+        window.innerWidth < this.minWidthResolution;
+    });
+
+    // var ua=navigator.userAgent,tem;
+    const webBrowserInfo =
+      navigator.userAgent.match(
+        /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+      ) || [];
+    const browser = webBrowserInfo[1];
+    const browserVersion = parseInt(webBrowserInfo[2]);
+
+    this.incompatibleBrowsers.some((e: IncompatibleBrowserVersion) => {
+      //if version not between accepted versions, then show modal
+      if (
+        e.navigator == browser &&
+        (browserVersion < (e.minVersion || browserVersion) ||
+          browserVersion > (e.maxVersion || browserVersion))
+      ) {
+        this.showIncompatibleBrowserModal = true;
+      }
+      return this.showIncompatibleBrowserModal;
+    });
   }
 
   ngOnInit(): void {
