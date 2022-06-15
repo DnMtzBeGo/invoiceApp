b9060665c7ec22a555ca77426bf78ab549b1511d
Federico <federico@begomx.com>
2022-06-15 16:09:55 -0500

    Add tooltip to fleet members

--- src/app/shared/components/circular-avatar/circular-avatar.module.ts
+++ src/app/shared/components/circular-avatar/circular-avatar.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CircularAvatarComponent } from './circular-avatar.component';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [CircularAvatarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTooltipModule
   ],
   exports: [CircularAvatarComponent]
 })
