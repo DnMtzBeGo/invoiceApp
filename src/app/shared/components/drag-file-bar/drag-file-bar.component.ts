2cdbbf900642899f45c02c1d62a7fde9d038ac4b
JavierBego <javier@bego.ai>
2022-06-15 15:53:36 -0300

    feature-orders-drag_file-fix

--- src/app/shared/components/drag-file-bar/drag-file-bar.component.ts
+++ src/app/shared/components/drag-file-bar/drag-file-bar.component.ts
@@ -1,29 +1,29 @@
-import { _fixedSizeVirtualScrollStrategyFactory } from '@angular/cdk/scrolling';
-import { 
-  Component, 
-  ElementRef, 
-  EventEmitter, 
-  OnInit, 
+import { _fixedSizeVirtualScrollStrategyFactory } from "@angular/cdk/scrolling";
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
   ViewChild,
-   Output, 
-   Input, 
-  SimpleChange, 
-  SimpleChanges,  
-} from '@angular/core';
-import { Subject } from 'rxjs';
-import { FileInfo } from 'src/app/pages/profile/components/fiscal-docs/interfaces/FileInfo';
-import * as moment from 'moment';
+  Output,
+  Input,
+  SimpleChange,
+  SimpleChanges,
+} from "@angular/core";
+import { Subject } from "rxjs";
+import { FileInfo } from "src/app/pages/profile/components/fiscal-docs/interfaces/FileInfo";
+import * as moment from "moment";
 
 @Component({
-  selector: 'bego-drag-file-bar',
-  templateUrl: './drag-file-bar.component.html',
-  styleUrls: ['./drag-file-bar.component.scss']
+  selector: "bego-drag-file-bar",
+  templateUrl: "./drag-file-bar.component.html",
+  styleUrls: ["./drag-file-bar.component.scss"],
 })
 export class DragFileBarComponent implements OnInit {
+  @ViewChild("fileInput", { read: ElementRef, static: false }) fileInput!: ElementRef;
+  @ViewChild("uploadFilesBar", { read: ElementRef, static: false })
+  uploadFilesBar!: ElementRef;
 
-  @ViewChild('fileInput',{ read: ElementRef, static: false }) fileInput!: ElementRef;
-  @ViewChild('uploadFilesBar',{read: ElementRef, static: false}) uploadFilesBar!: ElementRef;
-  
   @Output() fileOutput = new EventEmitter<void>();
   @Output() deleteFile = new EventEmitter<void>();
 
@@ -31,83 +31,79 @@ export class DragFileBarComponent implements OnInit {
 
   //used to know if to show loaded file once it's loaded
   @Input() displayFileLoaded: boolean = false;
-  
 
   onChanges = new Subject<SimpleChanges>();
   file?: File;
   fileInfo?: FileInfo;
-  formattedDateUpdated?: string
-
+  formattedDateUpdated?: string;
 
   dragListeners = {
     dragOverEvent: this.dragOverEvent.bind(this),
     dragLeaveEvent: this.dragLeaveEvent.bind(this),
     dropListener: this.dropListener.bind(this),
-  }
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.fileInput);
   }
 
-  ngOnChanges(changes: SimpleChanges): void{
+  ngOnChanges(changes: SimpleChanges): void {
     this.onChanges.next(changes);
   }
 
-  clickFileInputElement(){
+  clickFileInputElement() {
+    console.log("clickFileInputElement");
     this.fileInput.nativeElement.click();
   }
 
-  dragOverEvent(event: any ){
+  dragOverEvent(event: any) {
     event.preventDefault();
-    event.target.classList.add('dragging-file');
+    event.target.classList.add("dragging-file");
   }
 
-  dragLeaveEvent(event: any){
+  dragLeaveEvent(event: any) {
     event.preventDefault();
-    event.target.classList.remove('dragging-file');
+    event.target.classList.remove("dragging-file");
   }
 
-  dropListener(event: any){
+  dropListener(event: any) {
     event.preventDefault();
     this.fileInput.nativeElement.files = event.dataTransfer.files;
-    event.target.classList.remove('dragging-file');
+    event.target.classList.remove("dragging-file");
     this.selectFile();
-
   }
 
-  addDragListeners(){
+  addDragListeners() {
     const uploadFilesBar = this.uploadFilesBar?.nativeElement;
 
-    if(uploadFilesBar){
+    if (uploadFilesBar) {
       uploadFilesBar.addEventListener("dragover", this.dragListeners.dragOverEvent);
-      uploadFilesBar.addEventListener('dragleave', this.dragListeners.dragLeaveEvent);
-      uploadFilesBar.addEventListener('drop', this.dragListeners.dropListener);
+      uploadFilesBar.addEventListener("dragleave", this.dragListeners.dragLeaveEvent);
+      uploadFilesBar.addEventListener("drop", this.dragListeners.dropListener);
     }
   }
 
-  removeDragListeners(){
-
+  removeDragListeners() {
     const uploadFilesBar = this.uploadFilesBar.nativeElement;
 
     uploadFilesBar.removeEventListener("dragover", this.dragListeners.dragOverEvent);
-    uploadFilesBar.removeEventListener('dragleave', this.dragListeners.dragLeaveEvent);
-    uploadFilesBar.removeEventListener('drop', this.dragListeners.dropListener);
+    uploadFilesBar.removeEventListener("dragleave", this.dragListeners.dragLeaveEvent);
+    uploadFilesBar.removeEventListener("drop", this.dragListeners.dropListener);
   }
 
-  ngAfterViewInit():void{
-
+  ngAfterViewInit(): void {
     //Listening when the disabled flag changes
-    this.onChanges.subscribe( ( change ) => {
-      if(change.disabled){
-        if(this.disabled){
+    this.onChanges.subscribe((change) => {
+      if (change.disabled) {
+        if (this.disabled) {
           this.removeDragListeners();
-        }else{
+        } else {
           this.addDragListeners();
         }
       }
     });
-
 
     this.addDragListeners();
   }
@@ -116,34 +112,31 @@ export class DragFileBarComponent implements OnInit {
    * Arranges information to be displayed in component if required
    * Emits 'fileOutput'
    */
-  selectFile():void{
+  selectFile(): void {
     const file = this.fileInput.nativeElement.files[0];
     this.fileInput.nativeElement.value = null;
-    const splitted = file.name.split('.');
+    const splitted = file.name.split(".");
     const extension = splitted.pop();
-    const text = splitted.join('.');
-    
+    const text = splitted.join(".");
+
     this.fileInfo = {
-      text, 
-      extension, 
+      text,
+      extension,
       key: file.name,
       file,
-      formattedSize: `${file.size/1000} kb`,
-    }
+      formattedSize: `${file.size / 1000} kb`,
+    };
 
-    file.size
+    file.size;
 
-    this.formattedDateUpdated = moment(file.lastModified).format('MMM dd YYYY');
+    this.formattedDateUpdated = moment(file.lastModified).format("MMM dd YYYY");
 
-    console.log('FileInfo: ', this.fileInfo, this.formattedDateUpdated);
+    console.log("FileInfo: ", this.fileInfo, this.formattedDateUpdated);
     this.fileOutput.emit(file);
-
   }
 
-  removeFile(): void{
+  removeFile(): void {
     this.fileInfo = undefined;
     this.deleteFile.emit();
   }
-
-
 }
