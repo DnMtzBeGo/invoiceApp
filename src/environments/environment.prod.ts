e4481c38b0ef035bb38be54865fe151333d554eb
Guillermo Rodriguez <guillermo@bego.ai>
2022-08-29 09:05:35 -0600

    environments updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   version: "v1.0.0",
-  URL_BASE: "https://bego-prod.herokuapp.com/v1.0/",
+  URL_BASE: "https://prod.bego.ai/v1.0/",
   SOCKET_URI: "https://SocketLB-1911102184.us-east-1.elb.amazonaws.com",
   website_url: "https://bego.ai/",
 };
