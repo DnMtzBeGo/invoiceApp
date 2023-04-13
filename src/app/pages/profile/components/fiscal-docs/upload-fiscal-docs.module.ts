d6fe64e29f141a316a9e35a9ef048c2dade2d670
Guillermo Rodriguez <guillermo@bego.ai>
2023-04-13 14:38:12 -0600

    removing mg-circle package from project for incompatibility with angular project version, improve error message when upload emitter files fails, implement math.div in scss

--- src/app/pages/profile/components/fiscal-docs/upload-fiscal-docs.module.ts
+++ src/app/pages/profile/components/fiscal-docs/upload-fiscal-docs.module.ts
@@ -6,12 +6,12 @@ import { DragFileBarModule } from 'src/app/shared/components/drag-file-bar/drag-
 import { FiscalDocumentCardComponent } from './components/fiscal-document-card/fiscal-document-card.component';
 import { UploadFiscalDocsComponent } from './upload-fiscal-docs.component';
 
-import { NgCircleProgressModule } from 'ng-circle-progress';
+//import { NgCircleProgressModule } from 'ng-circle-progress';
 import { LottieModule } from 'ngx-lottie';
 import player from 'lottie-web';
 import { ClickStopPropagationDirective } from '../../../../shared/directives/click-stop-propagation.directive';
 
-import { MatButtonModule } from "@angular/material/button";
+import { MatButtonModule } from '@angular/material/button';
 import { ButtonModule } from 'src/app/shared/components/button/button.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatSelectModule } from '@angular/material/select';
@@ -22,12 +22,7 @@ export function playerFactory() {
   return player;
 }
 @NgModule({
-  declarations: [
-    FiscalDocumentCardComponent,
-    FiscalDocumentItemComponent,
-    UploadFiscalDocsComponent,
-    ClickStopPropagationDirective,
-  ],
+  declarations: [FiscalDocumentCardComponent, FiscalDocumentItemComponent, UploadFiscalDocsComponent, ClickStopPropagationDirective],
   imports: [
     CommonModule,
     UploadFiscalDocsRoutingModule,
@@ -37,24 +32,24 @@ export function playerFactory() {
     TranslateModule,
     DragFileBarModule,
     MatSelectModule,
-    NgCircleProgressModule.forRoot({
-      "maxPercent":100,
-      "radius": 50,
-      "space": -6,
-      "outerStrokeWidth": 6,
-      "innerStrokeWidth": 6,
-      "outerStrokeColor": "#FFE000",
-      "innerStrokeColor": "#1e242b",
-      "showSubtitle":false,
-      "animation": true,
-      "titleFontSize": "40px",
-      "unitsFontSize": "20px",
-      "showBackground": false,
-      "clockwise": false,
-      "startFromZero": false,}),
-    LottieModule.forRoot({ player : playerFactory }),
+    // NgCircleProgressModule.forRoot({
+    //   maxPercent: 100,
+    //   radius: 50,
+    //   space: -6,
+    //   outerStrokeWidth: 6,
+    //   innerStrokeWidth: 6,
+    //   outerStrokeColor: '#FFE000',
+    //   innerStrokeColor: '#1e242b',
+    //   showSubtitle: false,
+    //   animation: true,
+    //   titleFontSize: '40px',
+    //   unitsFontSize: '20px',
+    //   showBackground: false,
+    //   clockwise: false,
+    //   startFromZero: false
+    // }),
+    LottieModule.forRoot({ player: playerFactory })
   ],
-  exports: [
-  ]
+  exports: []
 })
-export class UploadFiscalDocsModule { }
+export class UploadFiscalDocsModule {}
