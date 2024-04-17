47d3537dc21ff407b8cbb1d707bacece7d607cf7
IT <it@bego.ai>
2024-04-17 16:33:07 -0600

    Correct urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: "v1.1.0",
-  URL_BASE: "http://ec2-107-23-74-219.compute-1.amazonaws.com/v1.0/",
+  URL_BASE: "https://prod.bego.ai/v1.0/",
   SOCKET_URI: "https://slb.bego.ai/",
   website_url: "https://bego.ai/",
 };
