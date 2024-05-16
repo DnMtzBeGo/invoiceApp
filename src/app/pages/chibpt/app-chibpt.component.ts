cb432a266332d53ba46613d067594a426354211a
juliocesar-begomx <juliocesar@begomx.com>
2024-05-16 16:12:38 -0600

    Upgrade styles and change button for history

--- src/app/pages/chibpt/app-chibpt.component.ts
+++ src/app/pages/chibpt/app-chibpt.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-chibpt.component.scss']
 })
 export class AppChibptComponent {
-  isHistoryHidden: boolean = false;
+  public isHistoryHidden: boolean = false;
 
   
-  /*
+  
   messages = [
     { sender: 'user', content: 'Hello, how r u?'},
     { sender: 'bot', content: 'im great, what about you?' },
@@ -21,9 +21,9 @@ export class AppChibptComponent {
     { sender: 'user', content: 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
     { sender: 'bot', content: 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
   ];
-  */
+  
 
-  messages = [];
+  //messages = [];
 
   toggleHistory() {
     this.isHistoryHidden = !this.isHistoryHidden;
