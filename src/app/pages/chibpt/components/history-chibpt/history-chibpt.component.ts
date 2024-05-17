8c7d08a1be950d2c8a1ebe341e45ac2218c90cd4
joaquin-bg <joaquin@bego.ai>
2024-05-17 13:30:07 -0300

    added history chat EP

--- src/app/pages/chibpt/components/history-chibpt/history-chibpt.component.ts
+++ src/app/pages/chibpt/components/history-chibpt/history-chibpt.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-history-chibpt',
@@ -7,18 +8,36 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./history-chibpt.component.scss']
 })
 export class HistoryChibptComponent implements OnInit {
-
   @Input() openHistoryChat: boolean = false;
 
-  public lang:string = 'en';
+  public lang: string = 'en';
 
-  constructor(
-    private translateService: TranslateService
-  ) {
+  public histories = [];
+  public loading: boolean = false;
+
+  constructor(private translateService: TranslateService, private apiRestService: AuthService) {
     this.lang = this.translateService.currentLang;
   }
-  
-  public ngOnInit():void {
+
+  public async ngOnInit() {
+    await this.getHistoryChat();
   }
 
+  async getHistoryChat() {
+    this.loading = true;
+    (await this.apiRestService.apiRestGet(`assistant?limit=5&page=6`, { apiVersion: 'v1.1', loader: false })).subscribe({
+      next: ({ result }) => {
+        console.log(result);
+        if (result?.result?.length) this.histories = result.result;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    });
+  }
 }
