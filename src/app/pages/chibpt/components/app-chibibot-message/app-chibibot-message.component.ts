7fb0a3f5804dcbd68777ce893731ab5da5818c32
juliocesar-begomx <juliocesar@begomx.com>
2024-05-17 16:44:08 -0600

    add DomSanitizer and innerHTML for every message and install bego chat component with files

--- src/app/pages/chibpt/components/app-chibibot-message/app-chibibot-message.component.ts
+++ src/app/pages/chibpt/components/app-chibibot-message/app-chibibot-message.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 @Component({
   selector: 'app-app-chibibot-message',
   templateUrl: './app-chibibot-message.component.html',
   styleUrls: ['./app-chibibot-message.component.scss']
 })
-export class AppChibibotMessageComponent {
+export class AppChibibotMessageComponent implements OnInit {
 
   @Input() message: string;
+  formattedString: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.formattedString = this.sanitizer.bypassSecurityTrustHtml(this.message.replace(/\n/g,'<br>'));
+  }
+
+
 
 }
