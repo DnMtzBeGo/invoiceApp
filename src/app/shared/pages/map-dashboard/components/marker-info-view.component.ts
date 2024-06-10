717f114dd7032250fbe5ade00a0059ee7be5e800
joaquin-bg <joaquin@bego.ai>
2024-06-10 15:25:21 -0300

    added battery and speed info

--- src/app/shared/pages/map-dashboard/components/marker-info-view.component.ts
+++ src/app/shared/pages/map-dashboard/components/marker-info-view.component.ts
@@ -15,6 +15,8 @@ export class MarkerInfoWindowComponent implements OnInit {
   lastDate: string;
   location: string;
   loading: boolean = false;
+  speed: number;
+  battery: { width: string; color: string; percentage: string };
   @Output() errorLoadData = new EventEmitter<void>();
 
   constructor(public apiRestService: AuthService, private notificationsService: NotificationsService) {}
@@ -28,16 +30,19 @@ export class MarkerInfoWindowComponent implements OnInit {
 
     (await this.apiRestService.apiRestGet(`carriers/information?user_id=${this.memberId}`)).subscribe({
       next: ({ result }) => {
-        let { raw_nickname, email, location_updated_at, location } = result;
+        let { raw_nickname, email, location_updated_at, location, speed_kms_ph, device_battery } = result;
 
         this.username = raw_nickname;
         this.email = email;
         this.location = location;
+        this.speed = speed_kms_ph;
 
         if (location_updated_at) {
           const date = DateTime.fromMillis(location_updated_at);
           location_updated_at = date.toFormat('dd/MM/yyyy HH:mm:ss');
         }
+
+        this.calculateBatteryWidth(device_battery);
 
         this.lastDate = location_updated_at;
         this.loading = false;
@@ -52,5 +57,23 @@ export class MarkerInfoWindowComponent implements OnInit {
         this.loading = false;
       }
     });
+  }
+
+  calculateBatteryWidth(percentage) {
+    percentage = percentage ? Math.round(percentage * 100) : 'N/A';
+
+    let width: string;
+    let color: string;
+
+    if (percentage === 'N/A') {
+      color = '#ccc';
+      width = '0%';
+    } else {
+      color = percentage >= 0 && percentage <= 20 ? '#EA2929' : '#7AFF59';
+      width = percentage + '%';
+    }
+
+    this.battery = { width, color, percentage };
+    console.log('markerbattery: ', this.battery);
   }
 }
