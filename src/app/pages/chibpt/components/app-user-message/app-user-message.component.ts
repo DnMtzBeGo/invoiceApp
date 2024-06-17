e2b303266d5523ee864d1a07aab54d4f0833d6bb
Dulce María Ramírez Rendon <dulce@MacBook-Air.local>
2024-06-17 13:25:05 -0700

    PDF href in user message has been implemented

--- src/app/pages/chibpt/components/app-user-message/app-user-message.component.ts
+++ src/app/pages/chibpt/components/app-user-message/app-user-message.component.ts
@@ -52,11 +52,13 @@ export class AppUserMessageComponent implements OnInit {
   classifyFiles() {
     this.imageFiles = this.files.filter(file => file.type.startsWith('image/')).map(file => {
       if(!file.url){
-        file.url = URL.createObjectURL(file)
+        file.url = URL.createObjectURL(file);
+        console.log("este es el archivo recien adjuntado con url temporal", this.file);
       }
       return file;
     });
     this.pdfFiles = this.files.filter(file => file.type === 'application/pdf');
+    console.log("estos mson los opdfs owo", this.pdfFiles);
   }
 
   onError() {
