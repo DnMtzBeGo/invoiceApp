31265294940d6d32e463173ad67bb279c815e649
Dulce María Ramírez Rendon <dulce@192.168.1.9>
2024-07-10 19:34:13 -0600

    app chibpt and history: transition width and transform translateX

--- src/app/pages/chibpt/app-chibpt.component.ts
+++ src/app/pages/chibpt/app-chibpt.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { PrimeService } from 'src/app/shared/services/prime.service';
 
