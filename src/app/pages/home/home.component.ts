3ddac3cbd0c1f716eca27b4315a93d74f924c51f
joaquin-bg <joaquin@bego.ai>
2024-07-22 15:54:14 -0300

    fixed eslint warnings

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { GoogleLocation } from 'src/app/shared/interfaces/google-location';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
+import { trigger, style, animate, transition } from '@angular/animations';
+
+import { GoogleLocation } from 'src/app/shared/interfaces/google-location';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { PlacesService } from 'src/app/shared/services/places.service';
 import { GoogleMapsService } from 'src/app/shared/services/google-maps/google-maps.service';
 import { HeaderService } from './services/header.service';
 import { OrderPreview } from '../orders/orders.component';
-import { Location } from '@angular/common';
 import { InputDirectionsComponent } from 'src/app/shared/components/input-directions/input-directions.component';
-import { trigger, style, animate, transition } from '@angular/animations';
 import { PrimeService } from 'src/app/shared/services/prime.service';
 import { LocationsService } from '../../services/locations.service';
 import { MapDashboardService } from 'src/app/shared/pages/map-dashboard/map-dashboard.service';
@@ -19,12 +20,14 @@ import { MapDashboardService } from 'src/app/shared/pages/map-dashboard/map-dash
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [
-    trigger('slideInFromBottom', [transition('void => *', [style({ transform: 'translateY(100%)' }), animate('500ms ease-out')])])
-  ]
+    trigger('slideInFromBottom', [
+      transition('void => *', [style({ transform: 'translateY(100%)' }), animate('500ms ease-out')]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
-  @ViewChild(InputDirectionsComponent) inputDirections: InputDirectionsComponent;
-  @Input() locations: GoogleLocation = {
+  @ViewChild(InputDirectionsComponent) public inputDirections: InputDirectionsComponent;
+  @Input() public locations: GoogleLocation = {
     pickup: '',
     dropoff: '',
     pickupLat: '',
@@ -32,29 +35,29 @@ export class HomeComponent implements OnInit {
     dropoffLat: '',
     dropoffLng: '',
     pickupPostalCode: 0,
-    dropoffPostalCode: 0
+    dropoffPostalCode: 0,
   };
-  datepickup: number;
-  datedropoff: number;
-  draftData: any;
-  orderPreview: OrderPreview;
-  headerTransparent: boolean = true;
-  showOrderDetails: boolean = false;
+  public datepickup: number;
+  public datedropoff: number;
+  public draftData: any;
+  public orderPreview: OrderPreview;
+  public headerTransparent: boolean = true;
+  public showOrderDetails: boolean = false;
 
-  orderType = 'FTL';
+  public orderType = 'FTL';
 
   public typeMap: string = 'home';
   public imageFromGoogle: any;
   public membersToAssigned: object = {};
   public userWantCP: boolean = false;
 
-  subs = new Subscription();
+  public subs = new Subscription();
 
-  showSidebar = true;
-  showCompleteModal = false;
+  public showSidebar = true;
+  public showCompleteModal = false;
 
-  isPrime = false;
-  showTrafficButton: boolean;
+  public isPrime = false;
+  public showTrafficButton: boolean;
 
   constructor(
     private router: Router,
@@ -64,14 +67,14 @@ export class HomeComponent implements OnInit {
     private location: Location,
     public primeService: PrimeService,
     private locationsService: LocationsService,
-    public mapDashboardService: MapDashboardService
+    public mapDashboardService: MapDashboardService,
   ) {
     this.subs.add(
       this.router.events.subscribe((res) => {
         if (res instanceof NavigationEnd && res.url.startsWith('/home')) {
           this.cleanup();
         }
-      })
+      }),
     );
 
     this.subs.add(this.mapDashboardService.getCoordinates.subscribe(() => this.getCoordinates()));
@@ -79,7 +82,7 @@ export class HomeComponent implements OnInit {
     this.restoreDraft();
   }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     const data = this.location.getState() as any;
 
     if (data?.showCompleteModal) {
@@ -100,11 +103,11 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  ngOnDestroy(): void {
+  public ngOnDestroy(): void {
     this.subs.unsubscribe();
   }
 
-  cleanup() {
+  public cleanup() {
     this.showOrderDetails = false;
     this.draftData = null;
     this.datepickup = 0;
@@ -118,16 +121,16 @@ export class HomeComponent implements OnInit {
       dropoffLat: '',
       dropoffLng: '',
       pickupPostalCode: 0,
-      dropoffPostalCode: 0
+      dropoffPostalCode: 0,
     };
   }
 
-  restoreDraft() {
+  public restoreDraft() {
     const data = this.location.getState() as any;
 
     if (!data.draft) {
       this.updateMap();
-      return
+      return;
     }
 
     this.draftData = data.draft;
@@ -148,38 +151,40 @@ export class HomeComponent implements OnInit {
     this.location.replaceState(''); // removing draft data once consuming
   }
 
-  updateMap() {
+  public updateMap() {
     if (this.mapDashboardService.activeFilter) return;
     this.mapDashboardService.getFleetDetails.next(false);
   }
 
-  async createDraft() {
+  public async createDraft() {
     const dropoffId = this.locations.place_id_dropoff;
 
     const draftPayload = {
       destinations: [
         {
           type: 'pickup',
-          location: await this.getLocationId(this.locations.place_id_pickup)
+          location: await this.getLocationId(this.locations.place_id_pickup),
         },
         {
           type: 'dropoff',
-          location: dropoffId ? await this.getLocationId(dropoffId) : undefined
-        }
+          location: dropoffId ? await this.getLocationId(dropoffId) : undefined,
+        },
       ],
       stamp: this.userWantCP,
       type: this.orderType,
       target: 'carriers',
-      origin: 'web'
+      origin: 'web',
     };
 
-    const req = await this.webService.apiRest(JSON.stringify(draftPayload), 'orders/create_draft', { apiVersion: 'v1.1' });
+    const req = await this.webService.apiRest(JSON.stringify(draftPayload), 'orders/create_draft', {
+      apiVersion: 'v1.1',
+    });
     const { result } = await req.toPromise();
 
     this.orderPreview = result;
   }
 
-  async showNewOrderCard() {
+  public async showNewOrderCard() {
     await this.createDraft();
     this.showOrderDetails = true;
     this.mapDashboardService.showPolygons = false;
@@ -189,13 +194,16 @@ export class HomeComponent implements OnInit {
   private async getLocationId(place_id: string): Promise<string> {
     const payload = { place_id };
 
-    const req = await this.webService.apiRestPut(JSON.stringify(payload), 'orders/locations', { apiVersion: 'v1.1', loader: false });
+    const req = await this.webService.apiRestPut(JSON.stringify(payload), 'orders/locations', {
+      apiVersion: 'v1.1',
+      loader: false,
+    });
 
     const res = await req.toPromise();
     return res.result._id;
   }
 
-  updateLocation() {
+  public updateLocation() {
     if (this.mapDashboardService.activeFilter) {
       this.clearMap();
       this.mapDashboardService.clearFilter.next();
@@ -203,20 +211,20 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  updateLocations(data: GoogleLocation) {
+  public updateLocations(data: GoogleLocation) {
     this.locations = { ...data };
     this.mapDashboardService.showFleetMap = Boolean(!data.pickup || !data.dropoff);
   }
 
-  updateDatepickup(data: number) {
+  public updateDatepickup(data: number) {
     this.datepickup = data;
   }
 
-  updateDropOffDate(data: number) {
+  public updateDropOffDate(data: number) {
     this.datedropoff = data;
   }
 
-  getGoogleImageMap(data: any) {
+  public getGoogleImageMap(data: any) {
     this.imageFromGoogle = data;
   }
 
@@ -228,11 +236,11 @@ export class HomeComponent implements OnInit {
     this.userWantCP = event;
   }
 
-  onStepChange(step: number) {
+  public onStepChange(step: number) {
     this.showSidebar = !this.showOrderDetails || step < 3;
   }
 
-  getCoordinates() {
+  public getCoordinates() {
     if (this.inputDirections.autocompleteDropoff.input || this.inputDirections.autocompletePickup.input) {
       this.mapDashboardService.showFleetMap = true;
       this.inputDirections.ClearAutocompleteDropoff();
@@ -243,25 +251,30 @@ export class HomeComponent implements OnInit {
     this.mapDashboardService.openOrderMenu = false;
   }
 
-  clearedFilter() {
-    if (!this.mapDashboardService.activeFilter && (this.inputDirections.autocompleteDropoff.input || this.inputDirections.autocompletePickup.input)) return;
+  public clearedFilter() {
+    if (
+      !this.mapDashboardService.activeFilter &&
+      (this.inputDirections.autocompleteDropoff.input || this.inputDirections.autocompletePickup.input)
+    )
+      return;
     this.clearMap();
     this.mapDashboardService.activeFilter = false;
     this.mapDashboardService.getFleetDetails.next(false);
   }
 
-  clearMap() {
+  public clearMap() {
     this.mapDashboardService.clearMap.next();
     this.mapDashboardService.openOrderMenu = true;
   }
 
-  reloadMap() {
+  public reloadMap() {
     if (this.mapDashboardService.activeFilter) this.mapDashboardService.reloadPolygons.next();
     else this.mapDashboardService.getFleetDetails.next(false);
   }
 
-  centerMap() {
-    if (this.mapDashboardService.showFleetMap) this.mapDashboardService.centerMap.next(this.mapDashboardService.activeFilter);
+  public centerMap() {
+    if (this.mapDashboardService.showFleetMap)
+      this.mapDashboardService.centerMap.next(this.mapDashboardService.activeFilter);
     else this.mapDashboardService.centerRouteMap.next();
   }
 }
--- src/app/shared/pages/map-dashboard/map-dashboard.module.ts
+++ src/app/shared/pages/map-dashboard/map-dashboard.module.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BegoIconsModule, BegoPolygonsMapModule } from '@begomx/ui-components';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+
 import { FleetModule } from 'src/app/pages/fleet/fleet.module';
 import { PolygonFilterModule } from 'src/app/pages/home/components/polygon-filter/polygon-filter.module';
 import { HomeModule } from 'src/app/pages/home/home.module';
@@ -7,10 +12,6 @@ import { MapModule } from 'src/app/shared/components/map/map.module';
 import { MapDashboardRoutingModule } from './map-dashboard-routing.module';
 import { MapDashboardComponent } from './map-dashboard.component';
 import { MarkerInfoWindowComponent } from './components/marker-info-view.component';
-import { BegoIconsModule, BegoPolygonsMapModule } from '@begomx/ui-components';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { TranslateModule } from '@ngx-translate/core';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MapDashboardComponent, MarkerInfoWindowComponent],
