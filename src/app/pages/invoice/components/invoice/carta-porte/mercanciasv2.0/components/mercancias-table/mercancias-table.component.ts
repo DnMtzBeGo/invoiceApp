a4f09f0e70e9fd8116d42addee2324f471047ca8
Guillermo Rodriguez <guillermo@bego.ai>
2024-11-28 11:20:27 -0700

    fix: eslint errors

--- src/app/pages/invoice/components/invoice/carta-porte/mercanciasv2.0/components/mercancias-table/mercancias-table.component.ts
+++ src/app/pages/invoice/components/invoice/carta-porte/mercanciasv2.0/components/mercancias-table/mercancias-table.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { BegoChibiAlert, BegoDialogService } from '@begomx/ui-components';
 
 import { Paginator } from 'src/app/pages/invoice/models';
 import { CartaPorteInfoService } from '../../../services/carta-porte-info.service';
 import { generateUUID } from 'src/app/pages/invoice/containers/factura-edit-page/factura.core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ImportMerchandiseComponent } from './components/import-merchandise/import-merchandise.component';
-import { BegoChibiAlert, BegoDialogService } from '@begomx/ui-components';
 import { CartaPortePageComponent } from 'src/app/pages/invoice/containers';
 
 @Component({
