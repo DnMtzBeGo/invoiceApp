2e44af10efd64563b576ad5f1e14327fe04d1c83
Guillermo Rodriguez <guillermo@bego.ai>
2025-04-03 10:00:32 -0600

    multiple payment taxes and taxes totals sync

--- src/app/pages/invoice/components/multiple-payment-modal/enums.ts
+++ src/app/pages/invoice/components/multiple-payment-modal/enums.ts
@@ -4,6 +4,14 @@ export enum TaxFactor {
   Exento = 'Exento',
 }
 
-export enum TaxType {
+export enum TaxCodes {
   IVA = '002',
+  ISR = '003',
+  IEPS = '004',
+}
+
+export enum IVARates {
+  'P0' = 0,
+  'P08' = 0.08,
+  'P16' = 0.16,
 }
